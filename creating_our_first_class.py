8437de6c23914e5931736faae2c303f9941007fb
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-06-22 23:16:33 -0400

    create a basic class & an instance of that class, also add simple example to understand classes & instances

---
+++ creating_our_first_class.py
@@ -0,0 +1,19 @@
+# Keep this in mind: creating the instance == creating an object
+
+
+# 1st part: Create the class
+
+class Item:
+    pass # this is just to avoid errors for now
+
+# create the instance of the class
+item1 = Item()
+
+
+# # this example below is equivalent to creating an instance of a class {above} (in this case the class is <str>)
+# random_str = str(4)
+# print(type(random_str))
+
+
+
+# 2nd part: Create the instance of the class
