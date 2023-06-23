5352d987bd45ca68d2dd61845f82557a3bbcd242
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-06-22 23:28:41 -0400

    Print to check the dataType of the variables & notice the result difference from the intro file

--- creating_our_first_class.py
+++ creating_our_first_class.py
@@ -8,7 +8,18 @@ class Item:
 
 # create the instance of the class
 item1 = Item()
+# create attributes to one instance of the class
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
+# the way these are different from regular variables we created in intro_oop.py is in this way: all of these have a relationship, as these attributes are assigned to one instance of the class (therefore connecting them).
 
+# Print to check the dataType of the variables:
+print(type(item1))  # item
+# Notice how the dataType of item1 is now item (that's different from before)
+print(type(item1.name))  # str
+print(type(item1.price))  # int
+print(type(item1.quantity))  # int
 
 # # this example below is equivalent to creating an instance of a class {above} (in this case the class is <str>)
 # random_str = str(4)
